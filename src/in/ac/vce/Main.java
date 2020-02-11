aec8c36f989e8b89cd5e9b037010c8eab0162010
AparnaDarbha <aparnaadarbha@gmail.com>
2020-02-11 19:27:06 +0530

    first commit

---
+++ src/in/ac/vce/Main.java
@@ -0,0 +1,9 @@
+package in.ac.vce;
+public class Main {
+
+    public static void main(String[] args) {
+        CalculatorUI calculatorUI = new CalculatorUI();
+        calculatorUI.setVisible(true);
+        calculatorUI.setSize(800, 800);
+    }
+}
